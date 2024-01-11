efd048316e6a629cb5c8385aeadf8849d9d9e460
OL EEVIN <138324008+oleevin@users.noreply.github.com>
2024-01-11 20:06:36 +0700

    tesk1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
+class Rectangle {
+    protected double length;
+    protected double width;
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+    public double getArea() {
+        return length * width;
+    }
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+}
+class Square extends Rectangle {
+    public Square(double side) {
+        super(side, side);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+
+        Rectangle rectangle = new Rectangle(10, 50);
+        System.out.println("Area of rectangle: " + rectangle.getArea());
+        System.out.println("Perimeter of rectangle: " + rectangle.getPerimeter());
+
+        Square square = new Square(10);
+        System.out.println("Area of square: " + square.getArea());
+        System.out.println("Perimeter of square: " + square.getPerimeter());
+
     }
 }
